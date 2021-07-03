a4080ea8609e63c5fa8fb0d30f5c66c793e15444
abdul-rehman-2050 <abdul.rehman.vud@gmail.com>
2021-07-03 17:10:55 +0500

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import './style.css';
 // or less ideally
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { Nav, Form, FormControl } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 export default function App() {
   return (
-    <div className="container">
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <Button className="btn btn-danger">Hello </Button>
-    </div>
+    <>
+      <Navbar className="bg-danger hidden-xs" variant="dark">
+        <Navbar.Brand href="#home">RepairA</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link Link="#home">Home</Nav.Link>
+          <Nav.Link Link="#features">Features</Nav.Link>
+          <Nav.Link Link="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Navbar>
+      <div className="container">
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+        <Button className="btn btn-danger">Hello </Button>
+        <FontAwesomeIcon icon={faCoffee} />
+      </div>
+    </>
   );
 }
